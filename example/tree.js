bd1ac01e0a1c678e8ef27a3242749542e9c46f82
eshavrov <shavrov.ep@gmail.com>
2019-07-17 13:06:05 +0300

    Updated example

--- example/tree.js
+++ example/tree.js
@@ -1,93 +1,418 @@
 module.exports = {
-  module: 'react-ui-tree',
+  nodrag: true,
+  title: { text: "Тестовая книга", type: "name" },
   children: [
     {
-      module: 'dist',
-      collapsed: true,
+      __id: "8VEX8N3f",
+      parentId: "8mLJeCfw",
+      disabled: false,
+      children: [],
+      title: { text: "Раздел 1", type: "tl" },
+    },
+    {
+      __id: "pmI1l7UY",
+      parentId: "8mLJeCfw",
+      disabled: false,
+      children: [],
+      title: { text: "Примеры форматированого текста", type: "tl" },
+    },
+    {
+      __id: "CVfdeze9",
+      parentId: "8mLJeCfw",
+      disabled: false,
+      children: [],
+      title: { text: "Пример стихотворения", type: "tl" },
+    },
+    {
+      __id: "rbACN6CB",
+      parentId: "8mLJeCfw",
+      disabled: false,
+      children: [],
+      title: { text: "Таблица умножения", type: "tl" },
+    },
+    {
+      __id: "IifNb1S7",
+      parentId: "8mLJeCfw",
+      disabled: false,
+      children: [],
+      title: { text: "Илюстрации", type: "tl" },
+    },
+    {
+      __id: "xLHdylgM",
+      parentId: "8mLJeCfw",
+      disabled: false,
+      children: [],
+      title: { text: "Ссылки", type: "tl" },
+    },
+    {
+      __id: "ZLnCuz3z",
+      parentId: "8mLJeCfw",
+      disabled: false,
+      children: [],
+      title: { text: "Символы Unicode", type: "tl" },
+    },
+    {
+      __id: "ZcRTjnSZ",
+      parentId: "8mLJeCfw",
+      disabled: false,
+      children: [],
+      title: { text: "Глава 1", type: "tl" },
+    },
+    {
+      __id: "oOzuCJdq",
+      parentId: "8mLJeCfw",
+      disabled: false,
       children: [
         {
-          module: 'node.js',
-          leaf: true
+          __id: "SXSPuK4Z",
+          parentId: "oOzuCJdq",
+          disabled: false,
+          children: [],
+          title: {
+            text: "[Секция без заголовка. Далее – обычный текст]",
+            type: "p",
+          },
         },
         {
-          module: 'react-ui-tree.css',
-          leaf: true
+          __id: "f5x5IOOb",
+          parentId: "oOzuCJdq",
+          disabled: false,
+          children: [],
+          title: {
+            text: "[Эпиграф, вложенный в секцию без заголовка и",
+            type: "el",
+          },
         },
         {
-          module: 'react-ui-tree.js',
-          leaf: true
+          __id: "HqeRbjZP",
+          parentId: "oOzuCJdq",
+          disabled: false,
+          children: [],
+          title: {
+            text: "[Заголовок 1 ур., написанный в несколько стро",
+            type: "tl",
+          },
         },
         {
-          module: 'tree.js',
-          leaf: true
-        }
-      ]
+          __id: "7DgVXIbq",
+          parentId: "oOzuCJdq",
+          disabled: false,
+          children: [],
+          title: {
+            text: "[Заголовок_1_ур_состоящий_из_очень_длинного_с",
+            type: "tl",
+          },
+        },
+        {
+          __id: "wyJVgcPB",
+          parentId: "oOzuCJdq",
+          disabled: false,
+          children: [],
+          title: {
+            text: "[Заголовок 1 ур. с цитатой] Печорин признавал",
+            type: "tl",
+          },
+        },
+        {
+          __id: "GdJmU2jf",
+          parentId: "oOzuCJdq",
+          disabled: false,
+          children: [
+            {
+              __id: "8tqth7XV",
+              parentId: "GdJmU2jf",
+              disabled: false,
+              children: [],
+              title: { text: "[Заголовок 2 ур.]", type: "tl" },
+            },
+          ],
+          title: {
+            text: "[Заголовок 1 ур. (тестируем «лесенки» из заго",
+            type: "tl",
+          },
+        },
+        {
+          __id: "PYhNH0zq",
+          parentId: "oOzuCJdq",
+          disabled: false,
+          children: [
+            {
+              __id: "TRutWHq6",
+              parentId: "PYhNH0zq",
+              disabled: false,
+              children: [],
+              title: {
+                text: "[Заголовок 2 ур.] Из ряда вон выходящий генде",
+                type: "tl",
+              },
+            },
+            {
+              __id: "yX2cpBwb",
+              parentId: "PYhNH0zq",
+              disabled: false,
+              children: [],
+              title: {
+                text: "[Заголовок 2 ур.] Плюралистический радикал гл",
+                type: "tl",
+              },
+            },
+            {
+              __id: "MV1mkzJj",
+              parentId: "PYhNH0zq",
+              disabled: false,
+              children: [
+                {
+                  __id: "zVE34CNM",
+                  parentId: "MV1mkzJj",
+                  disabled: false,
+                  children: [],
+                  title: {
+                    text: "[Секция без заголовка. Далее – обычный текст]",
+                    type: "p",
+                  },
+                },
+                {
+                  __id: "GxbHiEtZ",
+                  parentId: "MV1mkzJj",
+                  disabled: false,
+                  children: [],
+                  title: {
+                    text: "[Заголовок 3 ур.] Неоднозначный узел – актуал",
+                    type: "tl",
+                  },
+                },
+                {
+                  __id: "T16pLukZ",
+                  parentId: "MV1mkzJj",
+                  disabled: false,
+                  children: [],
+                  title: {
+                    text: "[Заголовок 3 ур.] Ион-селективный памятник Ср",
+                    type: "tl",
+                  },
+                },
+                {
+                  __id: "E7wOlfPM",
+                  parentId: "MV1mkzJj",
+                  disabled: false,
+                  children: [
+                    {
+                      __id: "PFw8w7p5",
+                      parentId: "E7wOlfPM",
+                      disabled: false,
+                      children: [],
+                      title: {
+                        text: "[Заголовок 4 ур.] Концессия как ограниченная",
+                        type: "tl",
+                      },
+                    },
+                    {
+                      __id: "hK6mwhWh",
+                      parentId: "E7wOlfPM",
+                      disabled: false,
+                      children: [
+                        {
+                          __id: "8sEgrCPq",
+                          parentId: "hK6mwhWh",
+                          disabled: false,
+                          children: [],
+                          title: {
+                            text:
+                              "[Заголовок 5 ур.] Скалярное поле как прекрасн",
+                            type: "tl",
+                          },
+                        },
+                        {
+                          __id: "TSi6R7Ey",
+                          parentId: "hK6mwhWh",
+                          disabled: false,
+                          children: [
+                            {
+                              __id: "0pE2wz0C",
+                              parentId: "TSi6R7Ey",
+                              disabled: false,
+                              children: [],
+                              title: {
+                                text:
+                                  "[Заголовок 6 ур.] Эпизодический атом: предпос",
+                                type: "tl",
+                              },
+                            },
+                          ],
+                          title: {
+                            text:
+                              "[Заголовок 5 ур.] Межличностный интеграл Дири",
+                            type: "tl",
+                          },
+                        },
+                      ],
+                      title: {
+                        text: "[Заголовок 4 ур.] Почему косвенно поручительс",
+                        type: "tl",
+                      },
+                    },
+                  ],
+                  title: {
+                    text: "[Заголовок 3 ур.] Самодостаточный социально-п",
+                    type: "tl",
+                  },
+                },
+              ],
+              title: {
+                text: "[Заголовок 2 ур.] Вибрирующий официальный язы",
+                type: "tl",
+              },
+            },
+          ],
+          title: {
+            text: "[Заголовок 1 ур.] Глазами современников",
+            type: "tl",
+          },
+        },
+        {
+          __id: "OyCplG2t",
+          parentId: "oOzuCJdq",
+          disabled: false,
+          children: [],
+          title: {
+            text: "[Заголовок 1 ур. – секция с эпиграфом и аннот",
+            type: "tl",
+          },
+        },
+        {
+          __id: "NbbY1HMe",
+          parentId: "oOzuCJdq",
+          disabled: false,
+          children: [],
+          title: {
+            text: "[Заголовок 1 ур. – цитаты] Почему правомочен",
+            type: "tl",
+          },
+        },
+        {
+          __id: "9n6O7h9p",
+          parentId: "oOzuCJdq",
+          disabled: false,
+          children: [],
+          title: { text: "Маркированные списки", type: "tl" },
+        },
+        {
+          __id: "vFv9e4OD",
+          parentId: "oOzuCJdq",
+          disabled: false,
+          children: [],
+          title: { text: "Внешние ссылки", type: "tl" },
+        },
+      ],
+      title: { text: "Различные кейсы", type: "tl" },
     },
     {
-      module: 'example',
+      __id: "eRfi6cqA",
+      parentId: "8mLJeCfw",
+      disabled: false,
       children: [
         {
-          module: 'app.js',
-          leaf: true
+          __id: "kOgT9qRf",
+          parentId: "eRfi6cqA",
+          disabled: false,
+          children: [],
+          title: { text: "Глава 2.1", type: "tl" },
         },
         {
-          module: 'app.less',
-          leaf: true
+          __id: "qUNu2xwi",
+          parentId: "eRfi6cqA",
+          disabled: false,
+          children: [
+            {
+              __id: "Pmyx0SO0",
+              parentId: "qUNu2xwi",
+              disabled: false,
+              children: [],
+              title: { text: "Глава 2.2.1", type: "tl" },
+            },
+            {
+              __id: "IGiHJ9gd",
+              parentId: "qUNu2xwi",
+              disabled: false,
+              children: [],
+              title: { text: "Глава 2.2.2", type: "tl" },
+            },
+            {
+              __id: "yBowvAiu",
+              parentId: "qUNu2xwi",
+              disabled: false,
+              children: [
+                {
+                  __id: "jy8g5ii2",
+                  parentId: "yBowvAiu",
+                  disabled: false,
+                  children: [],
+                  title: { text: "Глава 2.2.3.1", type: "tl" },
+                },
+                {
+                  __id: "PcszJTwm",
+                  parentId: "yBowvAiu",
+                  disabled: false,
+                  children: [],
+                  title: { text: "Глава 2.2.3.2", type: "tl" },
+                },
+              ],
+              title: { text: "Глава 2.2.3", type: "tl" },
+            },
+          ],
+          title: { text: "Глава 2.2", type: "tl" },
         },
         {
-          module: 'index.html',
-          leaf: true
-        }
-      ]
-    },
-    {
-      module: 'lib',
-      children: [
-        {
-          module: 'node.js',
-          leaf: true
+          __id: "87t8VZOK",
+          parentId: "eRfi6cqA",
+          disabled: false,
+          children: [],
+          title: { text: "Глава 2.3", type: "tl" },
         },
-        {
-          module: 'react-ui-tree.js',
-          leaf: true
-        },
-        {
-          module: 'react-ui-tree.less',
-          leaf: true
-        },
-        {
-          module: 'tree.js',
-          leaf: true
-        }
-      ]
+      ],
+      title: { text: "Глава 2", type: "tl" },
     },
     {
-      module: '.gitiignore',
-      leaf: true
+      __id: "V5j7uzog",
+      parentId: "8mLJeCfw",
+      disabled: false,
+      children: [],
+      title: { text: "Глава 3", type: "tl" },
     },
     {
-      module: 'index.js',
-      leaf: true
+      __id: "cVQ4LWrc",
+      parentId: "8mLJeCfw",
+      disabled: false,
+      children: [],
+      title: { text: "Глава 4", type: "tl" },
     },
     {
-      module: 'LICENSE',
-      leaf: true
+      __id: "C3vNDeDd",
+      parentId: "8mLJeCfw",
+      disabled: false,
+      children: [],
+      title: { text: "Глава 5", type: "tl" },
     },
     {
-      module: 'Makefile',
-      leaf: true
+      __id: "DKHf0fo3",
+      parentId: "8mLJeCfw",
+      disabled: false,
+      children: [],
+      title: { text: "English example", type: "tl" },
     },
     {
-      module: 'package.json',
-      leaf: true
+      __id: "fWobj6TD",
+      parentId: "8mLJeCfw",
+      disabled: false,
+      children: [],
+      title: { text: "Polski przykład", type: "tl" },
     },
     {
-      module: 'README.md',
-      leaf: true
+      __id: "3k1P1Qhh",
+      parentId: "8mLJeCfw",
+      disabled: false,
+      children: [],
+      title: { text: "サンプルブック", type: "tl" },
     },
-    {
-      module: 'webpack.config.js',
-      leaf: true
-    }
-  ]
+  ],
 };
